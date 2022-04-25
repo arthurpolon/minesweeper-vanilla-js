b9a4ff8f49f112a34bde5d1cf264c6b863cb4a21
Your Name <arthur.lemos@afya.com.br>
2022-04-25 00:49:36 -0300

    📦 NEW: add click and mark logic in cells, reveal all nearby empty cells when click on a empty cell

--- cell.js
+++ cell.js
@@ -1,37 +1,91 @@
 import { createTwoDimensionalArray, mapTwoDimensionalArray } from './utils.js';
 
-function handleCellRightClick(event) {
-  event.preventDefault();
-  console.log('right clicked ' + event.target.dataset.row + ' ' + event.target.dataset.column);
+function markCell(cell) {
+  if (cell.isHidden) {
+    cell.isMarked = true;
+    cell.element.classList.add('marked');
+  }
 }
 
-function handleCellClick(event) {
-  console.log('clicked ' + event.target.dataset.row + ' ' + event.target.dataset.column);
+function unmarkCell(cell) {
+  cell.isMarked = false;
+  cell.element.classList.remove('marked');
 }
 
-function checkForNearbyBombs(row, column, cellsArray) {
-  let nearbyBombs = 0;
+function revealCell(cell) {
+  if (cell.isHidden && !cell.isMarked) {
+    cell.isHidden = false;
+    cell.element.classList.remove('hidden')
 
-  for(let offsetRow = -1; offsetRow <= 1; offsetRow++) {
-    for (let offsetColumn = -1; offsetColumn <= 1; offsetColumn++) {
-      if (offsetRow === 0 && offsetColumn === 0) continue;
+    if (cell.isBomb) {
+      cell.element.classList.add('bomb')
+      cell.element.innerHTML = '💣';
 
-      const currentCell = cellsArray[row + offsetRow]?.[column + offsetColumn];
+      //reveal all bombs
 
-      if (currentCell && currentCell.isBomb) {
-        nearbyBombs++;
-      }
+      // end game
+
+      return
+
+    }
+
+    cell.element.innerHTML = cell.neighborMines  ? cell.neighborMines : '';
+
+    if (cell.neighborMines === 0) {
+      cell.neighbors.forEach(neighbor => {
+        if (!neighbor.isBomb) {
+          neighbor.isMarked = false
+          neighbor.element.classList.remove('marked')
+        }
+
+        revealCell(neighbor);
+      });
     }
   }
+}
 
-  return nearbyBombs
+function handleCellRightClick(cell, event) {
+  event.preventDefault();
+
+  if (cell.isMarked) {
+    unmarkCell(cell);
+  } else {
+    markCell(cell)
+  }
+}
+
+function handleCellClick(cell) {
+  revealCell(cell)
+}
+
+function setCellsNeighbors(cellsArray) {
+  mapTwoDimensionalArray(cellsArray, (value, row, column) => {
+    for(let offsetRow = -1; offsetRow <= 1; offsetRow++) {
+      for (let offsetColumn = -1; offsetColumn <= 1; offsetColumn++) {
+        if (offsetRow === 0 && offsetColumn === 0) continue;
+
+        const currentCell = cellsArray[row + offsetRow]?.[column + offsetColumn];
+
+        if (currentCell) {
+          value.neighbors.push(currentCell);
+          value.neighborMines += currentCell.isBomb ? 1 : 0;
+        }
+      }
+    }
+  })
 }
 
 function createCell(row, column, bombsPosition) {
   const element = document.createElement('div');
 
   const cell = {
+    row,
+    column,
     element,
+    neighbors: [],
+    isHidden: true,
+    isMarked: false,
+    neighborMines: 0,
     isBomb: bombsPosition.some(bomb => bomb.row === row && bomb.column === column),
   }
 
@@ -39,24 +93,12 @@ function createCell(row, column, bombsPosition) {
   cell.element.dataset.column = column;
 
   cell.element.classList.add('cell');
-  if (cell.isBomb) cell.element.classList.add('bomb');
+  cell.element.classList.add('hidden');
 
-  cell.element.addEventListener('click', handleCellClick);
-  cell.element.addEventListener('contextmenu', handleCellRightClick);
+  cell.element.addEventListener('click', (event) => handleCellClick(cell, event));
+  cell.element.addEventListener('contextmenu', (event) => handleCellRightClick(cell, event));
 
   return cell;
-}
-
-function setCellsContent(cellsArray) {
-  mapTwoDimensionalArray(cellsArray, (value, row, column) => {
-    if (value.isBomb) {
-      value.element.innerHTML = '💣';
-    } else {
-      const nearbyBombs = checkForNearbyBombs(row, column, cellsArray);
-
-      value.element.innerHTML = nearbyBombs;
-    }
-  })
 }
 
 export function createCellsArray(boardSize, bombsPosition) {
@@ -67,7 +109,20 @@ export function createCellsArray(boardSize, bombsPosition) {
     return cell;
   });
 
-  setCellsContent(cellsArray);
+  setCellsNeighbors(cellsArray);
+
+  // setAllCellsContent(cellsArray);
 
   return cellsArray
+}
+
+function setAllCellsContent(cellsArray) {
+  mapTwoDimensionalArray(cellsArray, (cell, row, column) => {
+    if (cell.isBomb) {
+      cell.element.innerHTML = '💣';
+      cell.element.classList.add('bomb');
+    } else {
+      cell.element.innerHTML = cell.neighborMines;
+    }
+  })
 }
